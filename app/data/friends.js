5d6510cbba754a2d45f3697cdf4451ff2560ee58
Mthao09 <mthaoqk@users.noreply.github.com>
2018-07-23 23:03:46 -0500

    Alpha

---
+++ app/data/friends.js
@@ -0,0 +1,14 @@
+let friends = [{
+  name: "Magneto",
+  photo: 'https://i.pinimg.com/236x/7a/0a/11/7a0a112d9c9d86cac511639eb765b5c8--jim-lee-jim-orourke.jpg',
+  scores: [1,5,5,1,5,1,5,5,3,1],
+  message:"“Peace was never an option” "
+},
+{
+name: "Professor X",
+  photo: 'https://photos1.blogger.com/blogger/3841/751/1600/XMen01-Crosshatch.jpg',
+  scores: [5,1,1,5,1,5,1,1,5,5]
+  }
+];
+
+module.exports = friends;
